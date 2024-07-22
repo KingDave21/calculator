4635f87b0fd3bef0af3fcb5a3413f368b60bb301
KingDave21 <daydayy1200@gmail.com>
2024-07-22 16:48:47 +0000

    updated

--- app.js
+++ app.js
@@ -11,4 +11,34 @@ let decimalAdded = false
 
 const operations = [ "+", "-", "x", "/"]
 
-function handlekeypress(e){}
+function handlekeypress(e){
+    const key = e.target.dataset.key
+    const lastChar = operation[operation.length -1]
+
+    if (key === "="){
+        return
+    }
+    if (key === "." && decimalAdded){
+        return 
+    }
+
+    if(operations.indexOf(key) !== -1){
+        decimalAdded = false
+    }
+
+    if (operation.length === 0 && key === "-"){
+        operation += key 
+        input.innerHTML = operation
+        return
+    }
+
+    if (operation.length === 0 && operations.indexOf(key) !== -1) {
+        input.innerHTML = operation
+        return
+    }
+
+    if (operations.indexOf(lastChar) !== -1 && operations.indexOf(key) !== -1) {
+        input.innerHTML = operation
+        return
+    }
+}
